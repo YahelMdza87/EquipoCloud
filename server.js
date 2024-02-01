02dd7dc36b5e6a56f6201056048ecdf1c637e366
YahelMdza87 <yahelmendoza878@gmail.com>
2024-02-01 15:09:15 -0600

    Add server.js, add port3000

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get()
+
+
+const PORT = 3000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor ON en ${PORT}`);
+});
+
