4291e9bcb52661ef7133da4833403de0083e5aca
AlanMontes <alanjms15@gmail.com>
2024-02-20 22:31:16 -0600

    add jest-test module

---
+++ jest-test/jest.config.js
@@ -0,0 +1,10 @@
+const config = {
+        "displayName": "domocloud",
+        "globals" : {
+          "PROJECT_NAME" : "Domocloud"
+        },
+        "bail": 20,
+        "verbose": true
+}      
+
+module.exports = config;
