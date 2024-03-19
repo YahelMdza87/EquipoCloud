8f566b9ae39712ce676f04f8c3b219859c6cacef
AlanMontes <alanjms15@gmail.com>
2024-03-18 21:35:06 -0600

    Hide URL PostgreSQL

--- backEnd/config.js
+++ backEnd/config.js
@@ -1,1 +1,2 @@
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173"
+export const POSTGRE_URL = process.env.POSTGRE_URL || "postgres://admin:yuM7EwcKsPHI27gB536RselFhrPe46pL@dpg-cnoi2gacn0vc73bh8k20-a.oregon-postgres.render.com/domoticloud"
