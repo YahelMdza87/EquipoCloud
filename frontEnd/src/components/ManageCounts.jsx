83f2b3ce02e43cacdf136d671337f1e54d9391cc
AlanMontes <alanjms15@gmail.com>
2024-05-07 14:54:33 -0600

    Changes in admin files

--- frontEnd/src/components/ManageCounts.jsx
+++ frontEnd/src/components/ManageCounts.jsx
@@ -14,13 +14,19 @@ export default function ManageCounts (){
         navigate('/Principal')
       }
 
+      function toAdminMenu(){
+        navigate('/AdminMenu')
+    }
+
     return (
-      <div className="body-principal">
-          <div className="header-principal">
-              <h2 className="header-title-principal">Domoticloud</h2>
-              <img src={User} alt="" className="user-image-principal" onClick={toUserAccount}/>
-              <img src={Logo} alt="" className="add-icon-principal" onClick={toIndex}/>
-          </div>
-      </div>
+        <div className="body-principal">
+           <div className="admin-header">
+                <img src={Logo} alt="" className="admin-add-icon-principal" onClick={toIndex}/>
+                <h2 className="admin-header-title-menu" onClick={toAdminMenu}>Menu</h2>
+                <h2 className="admin-header-title-principal">Domoticloud</h2>
+                <h2 className="admin-header-title-user">Cuentas activas</h2>
+                <img src={User} alt="" className="admin-user-image-principal" onClick={toUserAccount}/>
+            </div>
+        </div>
     )
 }
